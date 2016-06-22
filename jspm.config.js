9d67b6d8ebec81dd08f353a04f691f52f3d6841b
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-22 10:25:42 +1000

    Install Mathbox via JSPM

---
+++ jspm.config.js
@@ -0,0 +1,38 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "bower:": "jspm_packages/bower/",
+    "mathbox-playground/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "mathbox-playground": {
+      "main": "mathbox-playground.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "bower:*.json"
+  ],
+  map: {
+    "mathbox": "bower:mathbox@0.0.5"
+  },
+  packages: {}
+});
