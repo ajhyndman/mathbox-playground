ba568a497deafacf7b923777fe42f50b46bb02c5
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-22 11:53:23 +1000

    Build Quick Start app
    https://github.com/unconed/mathbox/blob/master/docs/intro.md

--- src/app.js
+++ src/app.js
@@ -1,1 +1,127 @@
-document.getElementById('app').appendChild(new Text('Hello World!'));
+import { mathBox } from 'mathbox';
+
+
+// Bootstrap the mathBox instance.
+const myMathBox = mathBox({
+  element: document.querySelector('#app'),
+  init: true,
+  plugins: [
+    'core',
+  ],
+  renderer: {
+    parameters: {
+      alpha: true,
+    },
+  },
+  size: {
+    height: 500,
+  },
+});
+
+// Set up a camera.
+const camera = myMathBox.camera({
+  proxy: true,
+  position: [0, 0, 2],
+});
+
+// Map a cartesian space onto the visible canvas.
+const view = myMathBox
+  .cartesian({
+    range: [[-4, 4], [-2, 2]],
+    scale: [2, 1],
+  });
+
+view
+  .axis({
+    axis: 1,
+    width: 3,
+  })
+  .axis({
+    axis: 2,
+    width: 3,
+  })
+  .grid({
+    width: 1,
+    divideX: 41,
+    divideY: 21,
+  });
+
+// Tweak some view settings.
+myMathBox.select('axis').set('color', 'dark grey');
+myMathBox.select('grid').set('color', 'light grey');
+myMathBox.set('focus', 2);
+
+// Build some data.
+const data =
+  view.interval({
+    expr(emit, x, i, t) {
+      emit(x, Math.sin(x));
+    },
+    width: 41,
+    channels: 2,
+  });
+
+// Draw the data!
+const curve =
+  view.line({
+    width: 3,
+    color: '#3090FF',
+  });
+const points =
+  view.point({
+    size: 6,
+    color: '#3090FF',
+  });
+
+// Draw a second set of data.
+const vector =
+  view.interval({
+    expr(emit, x, i, t) {
+      emit(x, 0);
+      emit(x, 1 / Math.sin(x));
+    },
+    width: 41,
+    channels: 2,
+    items: 2,
+  })
+  .vector({
+    end: false,
+    width: 3,
+    color: '#50A000',
+  });
+
+// Assemble axis ticks and labels.
+const scale =
+  view.scale({
+    divide: 10,
+  });
+const ticks =
+  view.ticks({
+    width: 2,
+    size: 15,
+    color: 'dark grey',
+  });
+const format =
+  view.format({
+    digits: 2,
+    weight: 'normal',
+  });
+const labels =
+  view.label({
+    color: 'black',
+    zIndex: 1,
+  });
+
+
+// // Animate the entire scene.
+// const play = myMathBox.play({
+//   target: 'cartesian',
+//   pace: 5,
+//   to: 2,
+//   loop: true,
+//   script: [
+//     { props: { range: [[-2, 2], [-1, 1]] } },
+//     { props: { range: [[-4, 4], [-2, 2]] } },
+//     { props: { range: [[-2, 2], [-1, 1]] } },
+//   ],
+// });
